35f95985bf1027fb5360a45d233535c38719a6dd
unknown <diebraga0055@gmail.com>
2020-05-26 15:18:37 +0100

    prettier eslint and configured structure

---
+++ i-dont-know/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
+import GlobalStyles from './styles/globals';
+
+const App: React.FC = () => (
+  <>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+    <GlobalStyles />
+  </>
+);
+
+export default App;
---
+++ i-dont-know/src/main/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Main: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <h1>Hello</h1>
+        <hr />
+        <ul>
+          <li>Guys</li>
+        </ul>
+      </Container>
+    </>
+  );
+};
+
+export default Main;
