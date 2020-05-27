a0ccbb28682207aa528826ef3d8d117837c014c3
unknown <diebraga0055@gmail.com>
2020-05-27 14:30:20 +0100

    register page added fixed styles

---
+++ i-dont-know/src/_signup/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+import { TiCode } from 'react-icons/ti';
+import { FiArrowLeft, FiUser, FiLock, FiMail } from 'react-icons/fi';
+
+import Input from '../_components/input';
+import Button from '../_components/button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => {
+  return (
+    <>
+      <Container>
+        <Background />
+
+        <Content>
+          <TiCode size={200} />
+
+          <form>
+            <h1>Signup for free</h1>
+            <Input name="name" icon={FiUser} placeholder="name" />
+            <Input name="email" icon={FiMail} placeholder="E-mail" />
+            <Input
+              name="password"
+              icon={FiLock}
+              type="password"
+              placeholder="User name"
+            />
+            <Button type="submit">Submit</Button>
+            <br />
+            <br />
+          </form>
+          <br />
+
+          <a href="">
+            <FiArrowLeft />
+            <>I already have an account</>
+          </a>
+        </Content>
+      </Container>
+    </>
+  );
+};
+
+export default SignUp;
--- i-dont-know/src/routes/index.tsx
+++ i-dont-know/src/routes/index.tsx
@@ -3,11 +3,13 @@ import { Switch, Route } from 'react-router-dom';
 
 import SignIn from '../_signin';
 import Main from '../main';
+import SignUp from '../_signup';
 
 export const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Main} />
     <Route path="/login" component={SignIn} />
+    <Route path="/register" component={SignUp} />
   </Switch>
 );
 
