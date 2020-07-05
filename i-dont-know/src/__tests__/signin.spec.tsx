95714c4553bbaef23c71ff927eceb50706485890
unknown <diebraga0055@gmail.com>
2020-07-05 17:25:37 +0100

    input tests added

--- i-dont-know/src/__tests__/signin.spec.tsx
+++
@@ -1,6 +0,0 @@
-import react from 'react';
-import SignIn from '../_signin';
-
-describe('SignIn page', () => {
-  it('should be able to sign in');
-});
--- i-dont-know/src/_components/input/index.tsx
+++ i-dont-know/src/_components/input/index.tsx
@@ -42,7 +42,11 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   }, [fieldName, registerField]);
   return (
     <>
-      <Container isFilled={isFilled} isFocused={isFocused}>
+      <Container
+        isFilled={isFilled}
+        isFocused={isFocused}
+        data-testid="input-container"
+      >
         {Icon && <Icon size={20} />}
         <input
           onFocus={() => setFocused(true)}
