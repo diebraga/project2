92ebdbe5af849dda64c000cfdae5c76664ae0351
unknown <diebraga0055@gmail.com>
2020-07-06 23:15:16 +0100

    screenshots added

--- i-dont-know/src/__tests__/tests.spec.tsx
+++ i-dont-know/src/__tests__/tests.spec.tsx
@@ -6,6 +6,8 @@ import Card from '../_components/card';
 import Navbar from '../_components/navbar';
 import Footer from '../_components/footer';
 
+import SignIn from '../_signin';
+
 jest.mock('react-router-dom', () => {
   return {
     useHistory: jest.fn(),
@@ -95,5 +97,17 @@ describe('footer component', () => {
     const footerTestId = getByTestId('footer-container');
 
     expect(footerTestId).toBeTruthy();
+  });
+});
+
+describe(' component', () => {
+  it('should be able to render correctly', () => {
+    const { getByTestId, debug } = render(<SignIn />);
+
+    const footerTestId = getByTestId('signin-container');
+
+    expect(footerTestId).toBeTruthy();
+
+    debug();
   });
 });
